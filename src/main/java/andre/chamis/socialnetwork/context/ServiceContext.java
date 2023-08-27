3216884e7b915e03400cc6f79ed492c7cdfe3025
André <andrechamis21@gmail.com>
2023-08-27 13:58:00 -0300

    Added JavaDocs on ServiceContext related classes

--- src/main/java/andre/chamis/socialnetwork/context/ServiceContext.java
+++ src/main/java/andre/chamis/socialnetwork/context/ServiceContext.java
@@ -7,6 +7,9 @@ import org.slf4j.MDC;
 import java.time.Instant;
 import java.util.*;
 
+/**
+ * A utility class for managing contextual information within service operations.
+ */
 @Data
 @Slf4j
 public class ServiceContext {
@@ -26,10 +29,22 @@ public class ServiceContext {
     
     private Long sessionId;
 
+    /**
+     * Retrieves the current ServiceContext associated with the current thread. If no context exists, a new one is created.
+     *
+     * @return The current ServiceContext.
+     */
     public synchronized static ServiceContext getContext() {
         return getContext(null);
     }
 
+    /**
+     * Retrieves the current ServiceContext associated with the current thread, optionally specifying an execution ID.
+     * If no context exists, a new one is created.
+     *
+     * @param executionId An optional execution ID to associate with the context.
+     * @return The current ServiceContext.
+     */
     public synchronized static ServiceContext getContext(String executionId) {
         ServiceContext context = threadLocal.get();
         if (context == null){
@@ -39,6 +54,13 @@ public class ServiceContext {
         return context;
     }
 
+    /**
+     * Creates and associates a new ServiceContext with the current thread, optionally using the provided execution ID.
+     * If no execution ID is provided or is blank, a new UUID-based execution ID is generated.
+     *
+     * @param executionId An optional execution ID to associate with the context.
+     * @return The newly created ServiceContext.
+     */
     private static ServiceContext createNewContext(String executionId) {
         ServiceContext context = new ServiceContext(executionId);
         threadLocal.set(context);
@@ -46,6 +68,13 @@ public class ServiceContext {
         return context;
     }
 
+    /**
+     * Constructs a new ServiceContext with an execution ID. If the provided execution ID is null or blank,
+     * a new UUID-based execution ID is generated. The constructed context is associated with the current thread,
+     * and the execution ID is added to the Mapped Diagnostic Context (MDC) for logging purposes.
+     *
+     * @param executionId An optional execution ID to associate with the context.
+     */
     private ServiceContext(String executionId) {
         executionId = executionId == null || executionId.isBlank()
                 ? UUID.randomUUID().toString()
@@ -56,19 +85,37 @@ public class ServiceContext {
         MDC.put(EXECUTION_ID_KEY, executionId);
     }
 
+    /**
+     * Clears the context associated with the current thread.
+     */
     public static void clearContext(){
         threadLocal.remove();
         MDC.remove(EXECUTION_ID_KEY);
     }
 
+    /**
+     * Adds an exception to the list of exceptions in the current context.
+     *
+     * @param ex The exception to add.
+     */
     public synchronized static void addException(Exception ex) {
         getContext().exceptions.add(ex);
     }
 
+    /**
+     * Retrieves the metadata messages associated with the context.
+     *
+     * @return A set of metadata messages.
+     */
     public Set<String> getMetadataMessages() {
         return metadataMessages;
     }
 
+    /**
+     * Adds a message to the metadata messages associated with the context.
+     *
+     * @param message The message to add.
+     */
     public synchronized static void addMessage(String message){
         getContext().metadataMessages.add(message);
     }
--- src/main/java/andre/chamis/socialnetwork/interceptor/ServiceContextInterceptor.java
+++ src/main/java/andre/chamis/socialnetwork/interceptor/ServiceContextInterceptor.java
@@ -11,9 +11,20 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import java.time.Duration;
 import java.time.Instant;
 
+/**
+ * Interceptor responsible for managing the ServiceContext and logging execution details.
+ */
 @Slf4j
 @Component
 public class ServiceContextInterceptor implements HandlerInterceptor {
+    /**
+     * Pre-handle method that initializes the ServiceContext and logs the start of execution.
+     *
+     * @param request The incoming HTTP request.
+     * @param response The HTTP response.
+     * @param handler The handler for the request.
+     * @return {@code true} to proceed with further processing.
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,@NonNull Object handler) {
         String executionId = request.getHeader(ServiceContext.EXECUTION_ID_KEY);
@@ -32,6 +43,14 @@ public class ServiceContextInterceptor implements HandlerInterceptor {
         return true;
     }
 
+    /**
+     * After-completion method that logs execution details and clears the ServiceContext.
+     *
+     * @param request The incoming HTTP request.
+     * @param response The HTTP response.
+     * @param handler The handler for the request.
+     * @param ex The exception that occurred during processing (if any).
+     */
     @Override
     public void afterCompletion(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull Object handler, Exception ex)  {
         ServiceContext context = ServiceContext.getContext();
